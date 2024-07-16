03fa91bccbe668b4723e1200f768674cb7cd5f78
TrevorStreng <99687886+TrevorStreng@users.noreply.github.com>
2024-07-15 17:15:08 -0700

    added clear button, added item editing, added line-through on check, styling changes

--- app/page.js
+++ app/page.js
@@ -7,18 +7,27 @@ import { useState, useRef, useEffect } from "react";
 
 export default function Home() {
   const [items, setItems] = useState([]);
-
   const [showModal, setShowModal] = useState(false);
   const [newItem, setNewItem] = useState({
     name: "",
     category: "",
     checked: false,
   });
+  const [editingItem, setEditingItem] = useState({
+    categoryIndex: null,
+    itemIndex: null,
+  });
+  const [editValue, setEditValue] = useState("");
   const modalRef = useRef(null);
+  const editInputRef = useRef(null);
 
   const handleInputChange = (e) => {
     const { id, value } = e.target;
     setNewItem({ ...newItem, [id]: value });
+  };
+
+  const handleEditChange = (e) => {
+    setEditValue(e.target.value);
   };
 
   const addItem = (newItem) => {
@@ -79,6 +88,39 @@ export default function Home() {
     addItem(newItem);
   };
 
+  const handleEditSubmit = (categoryIndex, itemIndex) => {
+    const updatedItems = [...items];
+    updatedItems[categoryIndex].items[itemIndex].name = editValue;
+    setItems(updatedItems);
+    localStorage.setItem("items", JSON.stringify(updatedItems));
+    // setEditingItem({ categoryIndex: null, itemIndex: null });
+    // setEditValue("");
+  };
+
+  const clearCart = () => {
+    setItems([]);
+    localStorage.removeItem("items");
+  };
+
+  const toggleEditItem = (item, categoryIndex, itemIndex) => {
+    console.log(categoryIndex);
+    if (
+      editingItem.categoryIndex === categoryIndex &&
+      editingItem.itemIndex === itemIndex
+    ) {
+      console.log("herre");
+      setEditingItem({
+        categoryIndex: null,
+        itemIndex: null,
+      });
+      setEditValue("");
+    } else {
+      console.log("no");
+      setEditingItem({ categoryIndex, itemIndex });
+      setEditValue(item.name);
+    }
+  };
+
   useEffect(() => {
     const storedItems = localStorage.getItem("items");
     storedItems ? setItems([...JSON.parse(storedItems)]) : setItems([]);
@@ -102,25 +144,54 @@ export default function Home() {
     };
   }, [showModal]);
 
+  useEffect(() => {
+    const handleClickOutsideEditing = (event) => {
+      if (
+        editInputRef.current &&
+        !editInputRef.current.parentElement.parentElement.contains(event.target)
+      ) {
+        setEditingItem({ categoryIndex: null, itemIndex: null });
+        setEditValue("");
+      }
+    };
+
+    if (editValue) {
+      document.addEventListener("mousedown", handleClickOutsideEditing);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutsideEditing);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutsideEditing);
+    };
+  }, [editingItem]);
+
+  useEffect(() => {
+    if (editingItem.categoryIndex !== null && editingItem.itemIndex !== null) {
+      editInputRef.current.focus();
+    }
+  }, [editingItem]);
+
   return (
     <main className="w-screen h-screen">
       <div className="flex flex-col justify-center items-center">
-        <h1 className="pt-4 text-3xl font-bold">Cart2Go</h1>
-        <h2 className="p-4">Items</h2>
-        <div className="w-2/3">
+        <h1 className="p-4 text-3xl font-bold">Cart2Go</h1>
+        <div className="w-full">
           {items && items.length > 0 ? (
-            items.map((category, index) => (
+            items.map((category, categoryIndex) => (
               <div
-                key={index}
-                className="border-2 border-gray-200 rounded-2xl mb-4"
+                key={categoryIndex}
+                className={`border-2 border-gray-200 ${
+                  categoryIndex === 0 ? "border-b-0" : ""
+                }`}
               >
-                <h3 className="text-lg font-semibold bg-gray-100 rounded-t-2xl">
+                <h3 className="text-lg font-semibold bg-gray-100">
                   {category.category}
                 </h3>
                 {category.items.map((item, itemIndex) => (
                   <div
                     key={itemIndex}
-                    className={`flex items-center px-4 ${
+                    className={`flex w-full justify-between items-center px-4 ${
                       itemIndex === 0 ? "border-t-2" : ""
                     } ${
                       itemIndex === category.items.length - 1
@@ -128,21 +199,49 @@ export default function Home() {
                         : "border-b-2"
                     } h-16 py-2`}
                   >
-                    <span
-                      className={`rounded-full w-4 h-4 border-gray-300 border-2 ${
-                        item.checked ? "bg-green-600" : ""
-                      }`}
-                      onClick={() => checkItem(category, item)}
-                    ></span>
-                    <p className="ml-2">{item.name}</p>
-                    {/* <p>{item.amount}</p> */}
+                    <div className="flex items-center text-lg">
+                      <span
+                        className={`rounded-full w-4 h-4 border-gray-300 border-2 ${
+                          item.checked ? "bg-green-600" : ""
+                        }`}
+                        onClick={() => checkItem(category, item)}
+                      ></span>
+                      {editingItem.categoryIndex === categoryIndex &&
+                      editingItem.itemIndex === itemIndex ? (
+                        <input
+                          type="text"
+                          value={editValue}
+                          onChange={handleEditChange}
+                          onBlur={() =>
+                            handleEditSubmit(categoryIndex, itemIndex)
+                          }
+                          ref={editInputRef}
+                          className="ml-2 border border-gray-300 rounded px-2"
+                        />
+                      ) : (
+                        <p
+                          className={`ml-2 ${
+                            item.checked ? "line-through text-slate-400" : ""
+                          }`}
+                        >
+                          {item.name}
+                        </p>
+                      )}
+                    </div>
+                    <img
+                      src="/pencil-edit-button-svgrepo-com.svg"
+                      className="w-5 cursor-pointer"
+                      onClick={() => {
+                        toggleEditItem(item, categoryIndex, itemIndex);
+                      }}
+                    ></img>
                   </div>
                 ))}
               </div>
             ))
           ) : (
             <div>
-              <p>add items</p>
+              <p>Your cart is empty!</p>
             </div>
           )}
         </div>
@@ -202,6 +301,9 @@ export default function Home() {
           </form>
         </div>
       )}
+      <div className="absolute top-0 right-0 p-2">
+        <button onClick={clearCart}>clear</button>
+      </div>
     </main>
   );
 }
